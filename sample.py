5e8827476440f31405916a4a43be185be8455633
Okkyyt <okky.161227@gmail.com>
2024-03-12 03:26:10 +0900

    sample-json

--- sample.py
+++ sample.py
@@ -1,35 +1,32 @@
-import csv
+import json
 
-# 変数の準備
-data = [
-    ['名前', '年齢', '職業'],
-    ['太郎', 30, 'エンジニア'],
-    ['花子', 25, 'デザイナー'],
-    ['次郎', 35, 'マネージャー']
-]
+def write_json(filename, data):
+    """JSON形式のデータを指定したファイルに書き込む関数"""
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file)
 
-# 変数をCSVファイルに書き込む関数
-def write_to_csv(filename, data):
-    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerows(data)
+def read_json(filename):
+    """指定したJSONファイルからデータを読み込む関数"""
+    with open(filename, 'r') as json_file:
+        loaded_data = json.load(json_file)
+    return loaded_data
 
-# CSVファイルから変数を読み込む関数
-def read_from_csv(filename):
-    with open(filename, 'r', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile)
-        data = [row for row in reader]
-    return data
+def delete_json_content(filename):
+    """指定したJSONファイルの内容を削除する関数"""
+    empty_data = {}
+    with open(filename, 'w') as json_file:
+        json.dump(empty_data, json_file)
 
-# 変数をテキスト化してCSVファイルに書き込む
-write_to_csv('data.csv', data)
+# 書き込むデータ
+data_to_write = {'key1': 'value1', 'key2': 'value2'}
 
-# CSVファイルから変数を読み込む
-loaded_data = read_from_csv('data.csv')
-print("Loaded Data:")
-for row in loaded_data:
-    print(row)
+# JSONファイルにデータを書き込む
+write_json('data.json', data_to_write)
 
-# CSVファイルの中身を削除する
-open('data.csv', 'w').close()
-print("CSV file content deleted.")
+# JSONファイルからデータを読み込む
+loaded_data = read_json('data.json')
+print("Loaded data:", loaded_data)
+
+# JSONファイルの内容を削除する
+delete_json_content('data.json')
+print("Content deleted.")
