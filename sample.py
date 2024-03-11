0168b5868703e3e8d78d841c0d3dd115614a5edc
Okkyyt <okky.161227@gmail.com>
2024-03-12 01:15:06 +0900

    json-sample

---
+++ sample.py
@@ -0,0 +1,32 @@
+import json
+
+def write_json(filename, data):
+    """JSON形式のデータを指定したファイルに書き込む関数"""
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file)
+
+def read_json(filename):
+    """指定したJSONファイルからデータを読み込む関数"""
+    with open(filename, 'r') as json_file:
+        loaded_data = json.load(json_file)
+    return loaded_data
+
+def delete_json_content(filename):
+    """指定したJSONファイルの内容を削除する関数"""
+    empty_data = {}
+    with open(filename, 'w') as json_file:
+        json.dump(empty_data, json_file)
+
+# 書き込むデータ
+data_to_write = {'key1': 'value1', 'key2': 'value2'}
+
+# JSONファイルにデータを書き込む
+write_json('data.json', data_to_write)
+
+# JSONファイルからデータを読み込む
+loaded_data = read_json('data.json')
+print("Loaded data:", loaded_data)
+
+# JSONファイルの内容を削除する
+delete_json_content('data.json')
+print("Content deleted.")
